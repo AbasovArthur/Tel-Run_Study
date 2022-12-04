fe737a9983417fdb3006b8ba0a10619a1c060acd
Arthur <abasov.ne@gmail.com>
2022-12-04 16:49:29 +0100

    add hw for Java & Linux

---
+++ Homework/Java/projectHW/hw4_1_12_22/src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    int a;
+    double b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public Test(int a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
